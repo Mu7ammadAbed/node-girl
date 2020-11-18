3ede057011c7ade24adbe8b64ec1f8a685a2d277
Mu7ammadAbed <mu7ammadabed@gmail.com>
2020-11-18 04:19:41 +0200

    Add process.env.port

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const http = require("http");
+const port = process.env.PORT || "3000";
 
 const router = require("./router");
 
 const server = http.createServer(router);
-const port = 3000;
 
 server.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
