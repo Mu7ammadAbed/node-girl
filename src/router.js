f1851ecb15a6c252077ecf3d3479c2feff71bc4d
Mu7ammadAbed <mu7ammadabed@gmail.com>
2020-11-18 10:45:31 +0200

    anything

--- src/router.js
+++ src/router.js
@@ -1,102 +1,100 @@
-const fs = require("fs");
-const path = require("path");
-const querystring = require("querystring");
+const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
 
 const router = (req, res) => {
   const endpoint = req.url;
-  let postsData = "";
-  if (endpoint === "/") {
-    const filePath = path.join(__dirname, "..", "public", "index.html");
+  if (endpoint === '/') {
+    const filePath = path.join(__dirname, '..', 'public', 'index.html');
     fs.readFile(filePath, (err, file) => {
       if (err) {
-        res.writeHead(500, { "Content-Type": "Text/html" });
-        res.write("<h1> Error 500: Internal Server Error");
+        res.writeHead(500, { 'Content-Type': 'Text/html' });
+        res.write('<h1> Error 500: Internal Server Error');
         res.end();
       } else {
-        res.writeHead(200, { "Content-Type": "Text/html" });
+        res.writeHead(200, { 'Content-Type': 'Text/html' });
         res.write(file);
         res.end();
       }
     });
-  } else if (endpoint.includes("public")) {
-    const filePath = path.join(__dirname, "..", ...endpoint.split("/"));
-    const fileType = endpoint.split(".")[1];
+  } else if (endpoint.includes('public')) {
+    const filePath = path.join(__dirname, '..', ...endpoint.split('/'));
+    const fileType = endpoint.split('.')[1];
     const MIMETypes = {
-      css: "text/css",
-      js: "application/js",
-      jpg: "image/jpeg",
-      png: "image/png",
-      ico: "image/x-icon",
+      css: 'text/css',
+      js: 'application/js',
+      jpg: 'image/jpeg',
+      png: 'image/png',
+      ico: 'image/x-icon',
     };
     fs.readFile(filePath, (err, file) => {
       if (err) {
-        res.writeHead(500, { "Content-Type": "Text/html" });
-        res.write("<h1> Error 500: Internal Server Error");
+        res.writeHead(500, { 'Content-Type': 'Text/html' });
+        res.write('<h1> Error 500: Internal Server Error');
         res.end();
       } else {
-        postsData += file;
-        res.writeHead(200, { "Content-Type": MIMETypes[fileType] });
+        res.writeHead(200, { 'Content-Type': MIMETypes[fileType] });
         res.write(file);
         res.end();
       }
     });
-  } else if (endpoint === "/posts") {
-    const filePath = path.join(__dirname, "posts.json");
+  } else if (endpoint === '/posts') {
+    const filePath = path.join(__dirname, 'posts.json');
     fs.readFile(filePath, (err, data) => {
       if (err) {
-        res.writeHead(500, { "Content-Type": "Text/html" });
-        res.write("<h1> Error 500: Internal Server Error");
+        res.writeHead(500, { 'Content-Type': 'Text/html' });
+        res.write('<h1> Error 500: Internal Server Error');
         res.end();
       } else {
-        res.writeHead(200, { "Content-Type": "application/json" });
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         res.write(data);
         res.end();
       }
     });
-  } else if (endpoint === "/create-post") {
-    let allTheData = "";
-    req.on("data", (chunkOfData) => {
+  } else if (endpoint === '/create-post') {
+    let allTheData = '';
+    req.on('data', (chunkOfData) => {
       allTheData += chunkOfData;
     });
-    req.on("end", () => {
+    req.on('end', () => {
       const convertedData = querystring.parse(allTheData);
-      res.writeHead(302, { Location: "/" });
-      const filePath = path.join(__dirname, "posts.json");
+      res.writeHead(302, { Location: '/' });
+      const filePath = path.join(__dirname, 'posts.json');
       fs.readFile(filePath, (err, data) => {
         if (err) {
-          res.writeHead(500, { "Content-Type": "Text/html" });
-          res.write("<h1> Error 500: Internal Server Error");
+          res.writeHead(500, { 'Content-Type': 'Text/html' });
+          res.write('<h1> Error 500: Internal Server Error');
           res.end();
         } else {
           try {
-            let timeStamp = Date.now();
+            const timeStamp = Date.now();
             const newData = {};
-            newData[timeStamp] = convertedData["post"];
+            newData[timeStamp] = convertedData.post;
             const fullData = {
               ...JSON.parse(data),
               ...newData,
             };
-            fs.writeFile(filePath, JSON.stringify(fullData), (err) => {
-              if (err) {
-                res.writeHead(500, { "Content-Type": "Text/html" });
-                res.write("<h1> Error 500: Internal Server Error");
+            fs.writeFile(filePath, JSON.stringify(fullData), (error) => {
+              if (error) {
+                res.writeHead(500, { 'Content-Type': 'Text/html' });
+                res.write('<h1> Error 500: Internal Server Error');
                 res.end();
               } else {
-                res.writeHead(302, { Location: "/" });
+                res.writeHead(302, { Location: '/' });
                 res.end();
               }
             });
-          } catch (err) {
-            res.writeHead(500, { "Content-Type": "Text/html" });
-            res.write("<h1> Error 500: Internal Server Error");
+          } catch (error) {
+            res.writeHead(500, { 'Content-Type': 'Text/html' });
+            res.write('<h1> Error 500: Internal Server Error');
             res.end();
           }
         }
       });
     });
   } else {
-    res.writeHead(404, { "Content-Type": "Text/html" });
-    res.write("<h1> Error 404: Not Found!");
+    res.writeHead(404, { 'Content-Type': 'Text/html' });
+    res.write('<h1> Error 404: Not Found!');
     res.end();
   }
 };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-const http = require("http");
-const port = process.env.PORT || "3000";
+const http = require('http');
+const env = require('env2')('.env');
 
-const router = require("./router");
+const port = process.env.PORT || 3000;
+const router = require('./router');
 
 const server = http.createServer(router);
 
 server.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is listening on http://localhost:${port}`);
 });
