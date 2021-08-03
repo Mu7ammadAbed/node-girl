0996356db417688d0bbf2a104def090d3838b83b
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2021-08-03 13:08:14 +0300

    fix bugs and update eslint

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const http = require('http');
 require('env2')('.env');
+const http = require('http');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require('./router');
 
 const server = http.createServer(router);
