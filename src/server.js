97fb63283925ecb0ed9eb518321c8edcd61bbb33
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2021-08-03 11:54:22 +0300

    Remove process.env for testing purposes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 require('env2')('.env');
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 const router = require('./router');
 
 const server = http.createServer(router);
